ff32e702743577fb4e1e5b42fd5f0284d93c877b
Greg <lupasco.grigori@gmail.com>
2022-04-07 22:03:48 +0300

    players and move functionality added

--- app/src/main/java/com/example/tictactoe/TicTac.kt
+++ app/src/main/java/com/example/tictactoe/TicTac.kt
@@ -5,15 +5,42 @@ import kotlin.math.abs
 
 fun main() {
 
-    print("Enter cells: ")
-    val board = readln().replace("_", " ")
+    val board = "         "
     val grid = arrayOf(
         arrayOf(board[0], board[1], board[2]),
         arrayOf(board[3], board[4], board[5]),
         arrayOf(board[6], board[7], board[8]),
     )
-    fieldPrint(grid)
 
+    fieldPrint(grid)
+    while (true) {
+
+        playerX(grid)
+        if (check(arrToString(grid)) == "X wins") {
+            println("X wins")
+            break
+        } else if (check(arrToString(grid)) == "O wins") {
+            println("O wins")
+            break
+        } else if (check(arrToString(grid)) == "Draw") {
+            println("Draw")
+            break
+        }
+        playerO(grid)
+        if (check(arrToString(grid)) == "X wins") {
+            println("X wins")
+            break
+        } else if (check(arrToString(grid)) == "O wins") {
+            println("O wins")
+            break
+        } else if (check(arrToString(grid)) == "Draw") {
+            println("Draw")
+            break
+        }
+    }
+}
+
+fun playerX(grid: Array<Array<Char>>): Array<Array<Char>> {
     var coords = gettingCords()
 
     var switch = true
@@ -27,10 +54,25 @@ fun main() {
             switch = false
         }
     }
-    check(arrToString(grid))
-    fieldPrint(grid)
+    return grid
 }
 
+fun playerO(grid: Array<Array<Char>>): Array<Array<Char>> {
+    var coords = gettingCords()
+
+    var switch = true
+    while (switch) {
+        if (grid[coords[0]][coords[1]] != ' ') {
+            println("This cell is occupied! Choose another one!")
+            coords = gettingCords()
+        } else {
+            grid[coords[0]][coords[1]] = 'O'
+            fieldPrint(grid)
+            switch = false
+        }
+    }
+    return grid
+}
 
 fun fieldPrint(mutList: Array<Array<Char>>) {
     println("-".repeat(9))
@@ -38,14 +80,12 @@ fun fieldPrint(mutList: Array<Array<Char>>) {
     println("-".repeat(9))
 }
 
-fun arrToString(grid: Array<Array<Char>>): String{
+fun arrToString(grid: Array<Array<Char>>): String {
     var result = ""
-    for (i in grid.indices)
-    {
+    for (i in grid.indices) {
         var j = 0
-        while (j < grid[i]?.size)
-        {
-            result+= grid[i][j].toString()
+        while (j < grid[i]?.size) {
+            result += grid[i][j].toString()
             j++
         }
     }
@@ -85,7 +125,7 @@ fun gettingCords(): List<Int> {
 }
 
 
-fun check(str: String) {
+fun check(str: String): String {
     val winsPositions = listOf(
         listOf(str[0], str[1], str[2]),
         listOf(str[3], str[4], str[5]),
@@ -105,13 +145,15 @@ fun check(str: String) {
     var x = 0
     var o = 0
 
+    var result = "String"
+
 
     for (i in str) {
         if (i == 'X') x++
         if (i == 'O') o++
     }
     val sp = 9 - x - o
-    val result = abs(x - o)
+    val xoProportion = abs(x - o)
 
     for (i in winsPositions) {
         if (i == playerX) xWinCount++
@@ -119,16 +161,17 @@ fun check(str: String) {
     }
     val winCount = xWinCount + oWinCount
 
-    if (result > 1 || winCount > 1) {
-        println("Impossible")
-    } else if (xWinCount == 1 && result <= 1) {
-        println("X wins")
-    } else if (oWinCount == 1 && result <= 1) {
-        println("O wins")
+    if (xoProportion > 1 || winCount > 1) {
+        result = "Impossible"
+    } else if (xWinCount == 1 && xoProportion <= 1) {
+        result = "X wins"
+    } else if (oWinCount == 1 && xoProportion <= 1) {
+        result = "O wins"
     } else if (sp == 0 && winCount == 0) {
-        println("Draw")
+        result = "Draw"
     } else if (sp > 0 && winCount == 0) {
-        println("Game not finished")
+        result = "Game not finished"
     }
 
+    return result
 }
